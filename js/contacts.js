fb2ce965b3996900ee26fba174b3f39d0b719052
Enes Geldi <geldi.dev@gmail.com>
2024-09-27 23:58:58 +0200

    add contacts view css

---
+++ js/contacts.js
@@ -0,0 +1,127 @@
+let contacts = [
+    {
+        "firstName": "Aaron",
+        "lastName": "Smith",
+        "email": "aaron.smith@example.com",
+        "phone": "+4912345678901"
+    },
+    {
+        "firstName": "Abigail",
+        "lastName": "Jones",
+        "email": "abigail.jones@example.com",
+        "phone": "+4912345678902"
+    },
+    {
+        "firstName": "Alex",
+        "lastName": "Brown",
+        "email": "alex.brown@example.com",
+        "phone": "+4912345678903"
+    },
+    {
+        "firstName": "Ben",
+        "lastName": "Taylor",
+        "email": "ben.taylor@example.com",
+        "phone": "+4912345678904"
+    },
+    {
+        "firstName": "Beth",
+        "lastName": "Davis",
+        "email": "beth.davis@example.com",
+        "phone": "+4912345678905"
+    },
+    {
+        "firstName": "Chris",
+        "lastName": "Miller",
+        "email": "chris.miller@example.com",
+        "phone": "+4912345678906"
+    },
+    {
+        "firstName": "David",
+        "lastName": "Wilson",
+        "email": "david.wilson@example.com",
+        "phone": "+4912345678907"
+    },
+    {
+        "firstName": "Ella",
+        "lastName": "Moore",
+        "email": "ella.moore@example.com",
+        "phone": "+4912345678908"
+    },
+    {
+        "firstName": "Finn",
+        "lastName": "Taylor",
+        "email": "finn.taylor@example.com",
+        "phone": "+4912345678909"
+    },
+    {
+        "firstName": "Grace",
+        "lastName": "Anderson",
+        "email": "grace.anderson@example.com",
+        "phone": "+4912345678910"
+    },
+    {
+        "firstName": "Hannah",
+        "lastName": "Thomas",
+        "email": "hannah.thomas@example.com",
+        "phone": "+4912345678911"
+    },
+    {
+        "firstName": "Isaac",
+        "lastName": "Jackson",
+        "email": "isaac.jackson@example.com",
+        "phone": "+4912345678912"
+    },
+    {
+        "firstName": "Jack",
+        "lastName": "White",
+        "email": "jack.white@example.com",
+        "phone": "+4912345678913"
+    },
+    {
+        "firstName": "Kara",
+        "lastName": "Martin",
+        "email": "kara.martin@example.com",
+        "phone": "+4912345678914"
+    },
+    {
+        "firstName": "Liam",
+        "lastName": "Garcia",
+        "email": "liam.garcia@example.com",
+        "phone": "+4912345678915"
+    },
+    {
+        "firstName": "Mia",
+        "lastName": "Martinez",
+        "email": "mia.martinez@example.com",
+        "phone": "+4912345678916"
+    },
+    {
+        "firstName": "Nina",
+        "lastName": "Robinson",
+        "email": "nina.robinson@example.com",
+        "phone": "+4912345678917"
+    },
+    {
+        "firstName": "Olivia",
+        "lastName": "Clark",
+        "email": "olivia.clark@example.com",
+        "phone": "+4912345678918"
+    },
+    {
+        "firstName": "Paul",
+        "lastName": "Lewis",
+        "email": "paul.lewis@example.com",
+        "phone": "+4912345678919"
+    },
+    {
+        "firstName": "Quinn",
+        "lastName": "Walker",
+        "email": "quinn.walker@example.com",
+        "phone": "+4912345678920"
+    }
+]
+
+
+function init() {
+    render
+}
