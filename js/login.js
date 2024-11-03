f163cf23fb72f869a14bd32dba9dd8faf26f0bc4
Michael Navarro <m.navarro@gmx.net>
2024-11-03 23:13:11 +0100

    js login updated

---
+++ js/login.js
@@ -0,0 +1,19 @@
+function changeImageWhenClickInputfield() {
+    let image = document.getElementById('lockImage');
+    image.src = "./assets/img/visibility_off.svg";
+    //let container = document.getElementById('signUp-input-password-container');
+    //container.classList.add('light-blue-border');
+}
+
+
+function makePasswordVisible() {
+    let passwordInput = document.getElementById('passwordInput');
+    let image = document.getElementById('lockImage');
+    if (image.src.includes("visibility_off.svg")) {
+        image.src = "./assets/img/visibility.svg";
+        passwordInput.type = "text";
+    } else {
+        image.src = "./assets/img/visibility_off.svg";
+        passwordInput.type = "password";
+    }
+}
