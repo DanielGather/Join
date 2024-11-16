818e7624dc167db438664eee836c583e069225a7
Enes Geldi <geldi.dev@gmail.com>
2024-11-16 12:01:23 +0100

    comment login.js

--- templates/contacts_temp.js
+++ templates/contacts_temp.js
@@ -1,3 +1,10 @@
+/**
+ * Generates an HTML template for a letter container.
+ * This container groups contacts alphabetically by their first letter.
+ * 
+ * @param {string} letter - The letter is the first letter of name used for grouping contacts.
+ * @returns {string} - HTML template string for the letter container.
+ */
 function temp_letterContainer(letter) {
     return /*html*/`
         <div id="containerOfLetter_${letter}">
@@ -7,9 +14,18 @@ function temp_letterContainer(letter) {
             <div class="contact-letter-seperator"></div>
             <div id="contactsOf_${letter}"></div>
         </div>
-    `
+    `;
 }
 
+/**
+ * Generates an HTML template for a contact card.
+ * Each contact card displays basic information like name, email, and initials, 
+ * and triggers a view function on click.
+ * 
+ * @param {string} id - Unique identifier for the contact.
+ * @param {Object} contact - Object containing the contact's details (e.g., name, email, initials).
+ * @returns {string} - HTML template string for the contact card.
+ */
 function temp_contactCard(id, contact) {
     return /*html*/`
         <div id="cardContact${id}" class="contact-s-card" onclick="viewContact('${id}')">
@@ -21,9 +37,18 @@ function temp_contactCard(id, contact) {
                 <span class="e-mail">${contact.email}</span>
             </div>
         </div>
-    `
+    `;
 }
 
+/**
+ * Generates an HTML template for a detailed contact information view.
+ * Displays the contact's profile picture, name, email, phone number, 
+ * and provides options for editing or deleting the contact.
+ * 
+ * @param {string} id - Unique identifier for the contact.
+ * @param {Object} contact - Object containing the contact's details (e.g., name, email, phone, initials).
+ * @returns {string} - HTML template string for the detailed contact view.
+ */
 function temp_contactInfo(id, contact) {
     return /*html*/`
         <div class="name-container">
@@ -38,17 +63,15 @@ function temp_contactInfo(id, contact) {
                         <span>Edit</span>
                     </a>
                     <a class="d-flex a-center g-12" href="#" onclick="deleteContact('${id}')">
-                        <svg width="16" height="18" viewBox="0 0 16 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M3 18C2.45 18 1.97917 17.8042 1.5875 17.4125C1.19583 17.0208 1 16.55 1 16V3C0.716667 3 0.479167 2.90417 0.2875 2.7125C0.0958333 2.52083 0 2.28333 0 2C0 1.71667 0.0958333 1.47917 0.2875 1.2875C0.479167 1.09583 0.716667 1 1 1H5C5 0.716667 5.09583 0.479167 5.2875 0.2875C5.47917 0.0958333 5.71667 0 6 0H10C10.2833 0 10.5208 0.0958333 10.7125 0.2875C10.9042 0.479167 11 0.716667 11 1H15C15.2833 1 15.5208 1.09583 15.7125 1.2875C15.9042 1.47917 16 1.71667 16 2C16 2.28333 15.9042 2.52083 15.7125 2.7125C15.5208 2.90417 15.2833 3 15 3V16C15 16.55 14.8042 17.0208 14.4125 17.4125C14.0208 17.8042 13.55 18 13 18H3ZM3 3V16H13V3H3ZM5 13C5 13.2833 5.09583 13.5208 5.2875 13.7125C5.47917 13.9042 5.71667 14 6 14C6.28333 14 6.52083 13.9042 6.7125 13.7125C6.90417 13.5208 7 13.2833 7 13V6C7 5.71667 6.90417 5.47917 6.7125 5.2875C6.52083 5.09583 6.28333 5 6 5C5.71667 5 5.47917 5.09583 5.2875 5.2875C5.09583 5.47917 5 5.71667 5 6V13ZM9 13C9 13.2833 9.09583 13.5208 9.2875 13.7125C9.47917 13.9042 9.71667 14 10 14C10.2833 14 10.5208 13.9042 10.7125 13.7125C10.9042 13.5208 11 13.2833 11 13V6C11 5.71667 10.9042 5.47917 10.7125 5.2875C10.5208 5.09583 10.2833 5 10 5C9.71667 5 9.47917 5.09583 9.2875 5.2875C9.09583 5.47917 9 5.71667 9 6V13Z"/>
-                        </svg>    
-                        <span>Delete</span>
+                    <svg width="16" height="18" viewBox="0 0 16 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M3 18C2.45 18 1.97917 17.8042 1.5875 17.4125C1.19583 17.0208 1 16.55 1 16V3C0.716667 3 0.479167 2.90417 0.2875 2.7125C0.0958333 2.52083 0 2.28333 0 2C0 1.71667 0.0958333 1.47917 0.2875 1.2875C0.479167 1.09583 0.716667 1 1 1H5C5 0.716667 5.09583 0.479167 5.2875 0.2875C5.47917 0.0958333 5.71667 0 6 0H10C10.2833 0 10.5208 0.0958333 10.7125 0.2875C10.9042 0.479167 11 0.716667 11 1H15C15.2833 1 15.5208 1.09583 15.7125 1.2875C15.9042 1.47917 16 1.71667 16 2C16 2.28333 15.9042 2.52083 15.7125 2.7125C15.5208 2.90417 15.2833 3 15 3V16C15 16.55 14.8042 17.0208 14.4125 17.4125C14.0208 17.8042 13.55 18 13 18H3ZM3 3V16H13V3H3ZM5 13C5 13.2833 5.09583 13.5208 5.2875 13.7125C5.47917 13.9042 5.71667 14 6 14C6.28333 14 6.52083 13.9042 6.7125 13.7125C6.90417 13.5208 7 13.2833 7 13V6C7 5.71667 6.90417 5.47917 6.7125 5.2875C6.52083 5.09583 6.28333 5 6 5C5.71667 5 5.47917 5.09583 5.2875 5.2875C5.09583 5.47917 5 5.71667 5 6V13ZM9 13C9 13.2833 9.09583 13.5208 9.2875 13.7125C9.47917 13.9042 9.71667 14 10 14C10.2833 14 10.5208 13.9042 10.7125 13.7125C10.9042 13.5208 11 13.2833 11 13V6C11 5.71667 10.9042 5.47917 10.7125 5.2875C10.5208 5.09583 10.2833 5 10 5C9.71667 5 9.47917 5.09583 9.2875 5.2875C9.09583 5.47917 9 5.71667 9 6V13Z"/>
+                    </svg>  
+                    <span>Delete</span>
                     </a>
                 </div>
             </div>
         </div>
-
         <span>Contact Information</span>
-
         <div class="column g-20">
             <div class="column g-15">
                 <h5>Email</h5>
@@ -62,9 +85,16 @@ function temp_contactInfo(id, contact) {
         <div class="mobile-menu-button center-center" onclick="openModal('mobileMenuDialog')">
             <img src="./assets/img/menu_dots.svg" alt="menu" />
         </div>
-    `
+    `;
 }
 
+/**
+ * Generates an HTML template for a mobile menu.
+ * Provides options for editing or deleting the contact, optimized for smaller screens.
+ * 
+ * @param {string} id - Unique identifier for the contact.
+ * @returns {string} - HTML template string for the mobile menu.
+ */
 function temp_mobileMenu(id) {
     return /*html*/`
         <section class="modal-content mobile-menu-content">
@@ -77,9 +107,9 @@ function temp_mobileMenu(id) {
             <a class="d-flex a-center g-11" href="#" onclick="deleteContact('${id}'), closeModal('mobileMenuDialog')">
                 <svg width="16" height="18" viewBox="0 0 16 18" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M3 18C2.45 18 1.97917 17.8042 1.5875 17.4125C1.19583 17.0208 1 16.55 1 16V3C0.716667 3 0.479167 2.90417 0.2875 2.7125C0.0958333 2.52083 0 2.28333 0 2C0 1.71667 0.0958333 1.47917 0.2875 1.2875C0.479167 1.09583 0.716667 1 1 1H5C5 0.716667 5.09583 0.479167 5.2875 0.2875C5.47917 0.0958333 5.71667 0 6 0H10C10.2833 0 10.5208 0.0958333 10.7125 0.2875C10.9042 0.479167 11 0.716667 11 1H15C15.2833 1 15.5208 1.09583 15.7125 1.2875C15.9042 1.47917 16 1.71667 16 2C16 2.28333 15.9042 2.52083 15.7125 2.7125C15.5208 2.90417 15.2833 3 15 3V16C15 16.55 14.8042 17.0208 14.4125 17.4125C14.0208 17.8042 13.55 18 13 18H3ZM3 3V16H13V3H3ZM5 13C5 13.2833 5.09583 13.5208 5.2875 13.7125C5.47917 13.9042 5.71667 14 6 14C6.28333 14 6.52083 13.9042 6.7125 13.7125C6.90417 13.5208 7 13.2833 7 13V6C7 5.71667 6.90417 5.47917 6.7125 5.2875C6.52083 5.09583 6.28333 5 6 5C5.71667 5 5.47917 5.09583 5.2875 5.2875C5.09583 5.47917 5 5.71667 5 6V13ZM9 13C9 13.2833 9.09583 13.5208 9.2875 13.7125C9.47917 13.9042 9.71667 14 10 14C10.2833 14 10.5208 13.9042 10.7125 13.7125C10.9042 13.5208 11 13.2833 11 13V6C11 5.71667 10.9042 5.47917 10.7125 5.2875C10.5208 5.09583 10.2833 5 10 5C9.71667 5 9.47917 5.09583 9.2875 5.2875C9.09583 5.47917 9 5.71667 9 6V13Z"/>
-                </svg>    
+                </svg> 
                 <span>Delete</span>
             </a>
         </section>
-    `
+    `;
 }
