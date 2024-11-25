3f632a69dea71860e7638045964fd268c2a023b1
Loew3nzahn <gather.daniel@yahoo.de>
2024-11-25 17:30:40 +0100

    css changes

--- js/dragAndDropMobile.js
+++ js/dragAndDropMobile.js
@@ -1,80 +1,80 @@
-let toDo, inProgress, awaitFeedback, done;
-let toDoPos, inProgressPos, awaitFeedbackPos, donePos;
+// let toDo, inProgress, awaitFeedback, done;
+// let toDoPos, inProgressPos, awaitFeedbackPos, donePos;
 
 
-const observer = new MutationObserver((mutationsList) => {
-  mutationsList.forEach((mutation) => {
-    if (mutation.type === "childList") {
-      const toDo = document.querySelector(".toDoArea");
-      const inProgress = document.querySelector(".progressArea");
-      const awaitFeedback = document.querySelector(".feedbackArea");
-      const done = document.querySelector(".doneArea");
-console.log("Done", done);
+// const observer = new MutationObserver((mutationsList) => {
+//   mutationsList.forEach((mutation) => {
+//     if (mutation.type === "childList") {
+//       const toDo = document.querySelector(".toDoArea");
+//       const inProgress = document.querySelector(".progressArea");
+//       const awaitFeedback = document.querySelector(".feedbackArea");
+//       const done = document.querySelector(".doneArea");
+// console.log("Done", done);
 
-      if (toDo && inProgress && awaitFeedback && done) {
-        console.log("Alle Elemente sind verfügbar");
-        toDoPos = toDo.getBoundingClientRect();
-        inProgressPos = inProgress.getBoundingClientRect();
-        awaitFeedbackPos = awaitFeedback.getBoundingClientRect();
-        donePos = done.getBoundingClientRect();
+//       if (toDo && inProgress && awaitFeedback && done) {
+//         console.log("Alle Elemente sind verfügbar");
+//         toDoPos = toDo.getBoundingClientRect();
+//         inProgressPos = inProgress.getBoundingClientRect();
+//         awaitFeedbackPos = awaitFeedback.getBoundingClientRect();
+//         donePos = done.getBoundingClientRect();
 
-        console.log("toDoPos:", toDoPos);
-        console.log("donePos:", donePos);
+//         console.log("toDoPos:", toDoPos);
+//         console.log("donePos:", donePos);
 
-        observer.disconnect();
-      }
-    }
-  });
-});
+//         observer.disconnect();
+//       }
+//     }
+//   });
+// });
 
-observer.observe(document.body, { childList: true, subtree: true });
+// observer.observe(document.body, { childList: true, subtree: true });
 
-function addStart(elem) {
-  console.log("Wird getriggert");
-  console.log("Füge touchmove Listener hinzu:", elem.target);
-  elem.target.addEventListener("touchstart", (e) => {
-    let startX = elem.changedTouches[0].clientX;
-    let startY = elem.changedTouches[0].clientY;
+// function addStart(elem) {
+//   console.log("Wird getriggert");
+//   console.log("Füge touchmove Listener hinzu:", elem.target);
+//   elem.target.addEventListener("touchstart", (e) => {
+//     let startX = elem.changedTouches[0].clientX;
+//     let startY = elem.changedTouches[0].clientY;
 
-    elem.target.addEventListener("touchend", (eve) => {
-      elem.target.style.zIndex = 0;
-      console.log("wir kommen auch hier rein");
-      console.log("Top Position",elem.target.getBoundingClientRect().top,"DonePos Top", donePos.top);
+//     elem.target.addEventListener("touchend", (eve) => {
+//       elem.target.style.zIndex = 0;
+//       console.log("wir kommen auch hier rein");
+//       console.log("Top Position",elem.target.getBoundingClientRect().top,"DonePos Top", donePos.top);
       
-      if (elem.target.getBoundingClientRect().top > donePos.top) {
-        console.log("Wir sind einen schritt weiter");
+//       if (elem.target.getBoundingClientRect().top > donePos.top) {
+//         console.log("Wir sind einen schritt weiter");
         
-        if (!done.contains(elem)) {
-          done.appendChild(elem);
-        }
-      } else if (elem.target.getBoundingClientRect().bottom < toDoPos.bottom) {
-        console.log("wir sind im else if");
+//         if (!done.contains(elem)) {
+//           done.appendChild(elem);
+//         }
+//       } else if (elem.target.getBoundingClientRect().bottom < toDoPos.bottom) {
+//         console.log("wir sind im else if");
         
-        if (!toDo.contains(elem)) {
-          toDo.appendChild(elem);
-        }
-      }
+//         if (!toDo.contains(elem)) {
+//           toDo.appendChild(elem);
+//         }
+//       }
 
-      elem.target.style.left = 0 + "px";
-      elem.target.style.top = 0 + "px";
-      console.log("Wir sind am Ende von touchEnd");
+//       elem.target.style.left = 0 + "px";
+//       elem.target.style.top = 0 + "px";
+//       console.log("Wir sind am Ende von touchEnd");
       
-    });
-    elem.target.addEventListener("touchmove", (eve) => {
-      console.log("Wir bewegen uns");
+//     });
+//     elem.target.addEventListener("touchmove", (eve) => {
+//       console.log("Wir bewegen uns");
       
-      eve.preventDefault();
-      let nextX = eve.changedTouches[0].clientX;
-      let nextY = eve.changedTouches[0].clientY;
-      // console.log(nextX,nextY);
+//       eve.preventDefault();
+//       let nextX = eve.changedTouches[0].clientX;
+//       let nextY = eve.changedTouches[0].clientY;
+//       // console.log(nextX,nextY);
       
-      elem.target.style.left = nextX - startX + "px";
-      elem.target.style.top = nextY - startY + "px";
-      console.log(elem.target.style.top, elem.target.style.left);
-      // console.log("Move Top Position",elem.target.getBoundingClientRect().top);
+//       elem.target.style.left = nextX - startX + "px";
+//       elem.target.style.top = nextY - startY + "px";
+//       console.log(elem.target.style.top, elem.target.style.left);
+//       // console.log("Move Top Position",elem.target.getBoundingClientRect().top);
       
       
-      elem.target.style.zIndex = 10;
-    });
-  });
-}
+//       elem.target.style.zIndex = 10;
+//     });
+//   });
+// }
--- templates/technicalTaskSmall_template.js
+++ templates/technicalTaskSmall_template.js
@@ -1,6 +1,6 @@
 function htmlTechnicalTaskSmall(element) {
   return /*HTML*/ `
-      <div id="${element[1]["id"]}" draggable="true" ondragstart="startDraggin(${element[1]["id"]}, event)" onclick="openTask(${element[1]["id"]})" ontouchstart="addStart(event)" class="d-flex dflex-col smallTask">
+      <div id="${element[1]["id"]}" draggable="true" ondragstart="startDraggin(${element[1]['id']}, event)" onclick="openTask(${element[1]['id']})" ontouchstart="addStart(event)" class="d-flex dflex-col small-task">
         <div style="display:flex; justify-content:space-between;"><div id="story${element[1]["id"]}" class="d-flex alic jc-c userStoryBackground br1 userStory"><span>${element[1]["story"]}</span></div>
           <div onclick="openMenu(event, ${element[1]['id']})" class="bars">
             <div class="bar"></div>
@@ -14,18 +14,18 @@ function htmlTechnicalTaskSmall(element) {
         <div class="d-flex colorGreyBg progress alic" style="width: 100%;">
           <div id="progressbar${element[1]["id"]}" class="d-flex colorBlueBg progress alic"></div>
         </div>
-        <span id="subTaskSmall${element[1]["id"]}" class="subTaskFont" style="width:100%"></span>
+        <span id="subTaskSmall${element[1]["id"]}" class="subtask-font" style="width:100%"></span>
       </div>
       <div class="d-flex jc-sb alic">
         <div id="assignedTo${element[1]["id"]}_taskSmall" class="d-flex pl035">
         </div>
         <div id="priority${element[1]["id"]}"><img  src="./assets/img/prioMedia.svg" alt="" /></div>
       </div>
-      <div id="menuContainer${element[1]["id"]}" class="menuContainer">
-      <div onclick="moveCategoryInFirebase(event,'toDo',${element[1]['id']})" class="menuButton">ToDo</div>
-      <div onclick="moveCategoryInFirebase(event,'inProgress',${element[1]['id']})" class="menuButton">inProgress</div>
-      <div onclick="moveCategoryInFirebase(event,'awaitFeedback',${element[1]['id']})" class="menuButton">AwaitFeedback</div>
-      <div onclick="moveCategoryInFirebase(event,'done',${element[1]['id']})" class="menuButton">Done</div>
+      <div id="menuContainer${element[1]["id"]}" class="menu-container">
+      <div onclick="moveCategoryInFirebase(event,'toDo',${element[1]['id']})" class="menu-button">ToDo</div>
+      <div onclick="moveCategoryInFirebase(event,'inProgress',${element[1]['id']})" class="menu-button">inProgress</div>
+      <div onclick="moveCategoryInFirebase(event,'awaitFeedback',${element[1]['id']})" class="menu-button">AwaitFeedback</div>
+      <div onclick="moveCategoryInFirebase(event,'done',${element[1]['id']})" class="menu-button">Done</div>
     </div>
     </div>
     
