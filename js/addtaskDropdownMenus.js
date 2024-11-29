1004aa5c60da025084629c04a3e382919a57f355
Michael Navarro <m.navarro@gmx.net>
2024-11-29 18:46:55 +0100

    js addTask updated

--- js/addtaskDropdownMenus.js
+++ js/addtaskDropdownMenus.js
@@ -39,3 +39,34 @@ function toggleCategoryDropDown() {
         imgCategoryDropdownToggle.src = './assets/img/arrow_drop_up.svg';
     }
 }
+
+/**
+ * Toggles the state of a checkbox when the surrounding container (div) is clicked,
+ * unless the click event directly targets the checkbox itself.
+ * 
+ * @param {Event} event - The click event triggered by the user interaction.
+ * @param {string} checkboxId - The unique ID of the checkbox to toggle.
+ * 
+ * @description
+ * This function checks if the click event was triggered on an element other than
+ * the checkbox (using the `event.target.tagName`). If so, it toggles the state
+ * (`checked` property) of the checkbox with the given `checkboxId`. Additionally,
+ * it calls the `renderContactInitials` function to handle any additional behavior
+ * associated with the checkbox's state change.
+ * 
+ * @param event.target.tagName
+ * - If the clicked element is not an "INPUT" (the checkbox), the function toggles
+ *   the checkbox's state.
+ * - Prevents duplicate toggling if the checkbox itself is clicked directly.
+ * 
+ * @param renderContactInitials
+ * - This function is called to handle additional updates or behavior when the
+ *   checkbox is toggled.
+ */
+function toggleContactCheckbox(event, checkboxId) {
+    if (event.target.tagName !== 'INPUT') {
+        const checkbox = document.getElementById(checkboxId);
+        checkbox.checked = !checkbox.checked;
+        renderContactInitials(checkboxId);
+    }
+}
--- templates/addTask_temp.js
+++ templates/addTask_temp.js
@@ -1,6 +1,6 @@
 function temp_generateHtmlAssignedToContacts(contact) {
     return /*html*/`
-    <div class="dropDownContacts" id="dropDownContacts">
+    <div class="dropDownContacts" id="dropDownContacts" onclick="toggleContactCheckbox(event, '${contact.email}')">
         <div class="initialNameContainer" id="initialNameContainer">
             <div class="initialsAssigned" id="initialsAssigned" style="background-color: ${contact.color}">${contact.initials}</div>
             <p id="contactNameAddTask">${contact.name}</p>
